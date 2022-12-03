e6e0beaea0920f90f6abe1269e642ff49c30e17a
Thaileaf <lethaison2019@gmail.com>
2022-12-03 16:20:33 -0500

    doing stuff

--- account.py
+++ account.py
@@ -4,6 +4,21 @@ import hashlib
 import sys
 from helperFuncs import *
 
+
+# Decorator to ensure routes can only be accessed
+# With correct credentials
+def login_required(func, login_state):
+    def check():
+        if login_state == "Customer":
+            if "email" in session:
+                return func();
+        elif login_state == "Staff":
+            if "username" in session:
+                return func();
+        else:
+            return redirect("/login");
+
+
 @app.route('/myaccount')
 def myaccount():
 	if "email" in session:
