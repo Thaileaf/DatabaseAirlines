cf2341df888d5d6e6f30c4e97919df810a34c545
aminoa <28660350+aminoa@users.noreply.github.com>
2022-12-08 16:38:57 -0500

    searching + buying tickets works

--- helperFuncs.py
+++ helperFuncs.py
@@ -73,6 +73,7 @@ def price_modify(airline_name, unique_airplane_num, flight_number, departure_dat
 
     if count_tickets // total_seats > 0.6:
         base_price *= 1.25
+        # base_price = int
 
     return base_price
 
@@ -216,33 +217,19 @@ def getComments( aName = None, fNum = None, dTime = None, dDate = None, aNum = N
     res = cursor.fetchall() 
     return res
 
-def userSearchFlight(departure_airport = None, arrival_airport = None, departure_city = None, arrival_city = None, departure_date = None, arrival_date = None, roundtrip_date = None):
+def userSearchFlight(departure_airport = None, arrival_airport = None, departure_date = None, roundtrip_date = None):
     query = 'SELECT * from flight where '
     cursor = conn.cursor()
     first_and = True
     
     if departure_airport:
         query += f' and depart_from = "{departure_airport}"'
-    elif departure_city:
-        # get the corresponding airport, then plug it in
-        airport_query = f'SELECT * from airport where city = "{departure_city}"'
-        cursor.execute(airport_query, (departure_city))
-        departure_airport = cursor.fetchone()
-        query += f' and depart_from = "{departure_airport}"'
-
     if arrival_airport:
         query += f' and arrive_at = "{arrival_airport}"'
-    elif arrival_city:
-        airport_query = f'SELECT * from airport where city = "{arrival_city}"'
-        cursor.execute(airport_query, (arrival_city))
-        arrival_airport = cursor.fetchone()
-        print(arrival_airport)
-        query += f' and arrive_art = "{arrival_airport}"'
-
     if departure_date:
         query += f' and departure_date = "{departure_date}"'
-    if arrival_date:
-        query += f' and arrival_date = "{arrival_date}"'
+    if roundtrip_date:
+        query += f' and arrival_date = "{roundtrip_date}"'
 
     query = query.replace("  and", "", 1)
     print(query)
